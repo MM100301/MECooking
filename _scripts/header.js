add0ffa23874e451c3e6e884a9ac0dcc0d76a534
MM100301 <matthew@themckinneys.com>
2025-02-12 18:38:35 -0500

    Testing js

--- _scripts/header.js
+++ _scripts/header.js
@@ -1,10 +1,13 @@
 $(function(){
     $(window).scroll(function(){
       var winTop = $(window).scrollTop();
+      console.log("the function is working");
       if(winTop >= 30){
         $("body").addClass("sticky-header");
+        console.log("keep header");
       }else{
         $("body").removeClass("sticky-header");
+        console.log("remove header");
       }//if-else
     });//win func.
   });//ready func.
