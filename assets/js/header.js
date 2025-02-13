07f48bd290739d1e5f73e471348ea46b22e36746
MM100301 <matthew@themckinneys.com>
2025-02-13 17:44:31 -0500

    change header threshhold

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM fully loaded and parsed");
 
-  const threshold = 100; // Set the scroll threshold
   const header = document.querySelector("header"); // Assuming you have a header element
 
   window.addEventListener("scroll", function() {
-    if (window.scrollY > threshold) {
+    if (window.scrollY > 30) {
       console.log("Removing sticky class");
       header.classList.remove("sticky");
     } else {
