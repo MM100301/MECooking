c8ca2c42bd182fcce02c7b5b0a0dade620283e06
Matthew McKinney <Matthew.McKinney@fcma.com>
2025-02-13 13:45:32 -0500

    again

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,13 +1,13 @@
 $(function(){
-    $(window).scroll(function(){
-      var winTop = $(window).scrollTop();
-      console.log("the function is working");
-      if(winTop >= 30){
-        $("body").addClass("sticky-header");
-        console.log("keep header");
-      }else{
-        $("body").removeClass("sticky-header");
-        console.log("remove header");
-      }
-    });
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop();
+    console.log("the function is working");
+    if(winTop >= 30){
+    $("body").css("color", "red"); // Change text color to red
+    console.log("keep header and change text color");
+    }else{
+    $("body").css("color", ""); // Reset text color
+    console.log("remove header and reset text color");
+    }
+  });
   });
